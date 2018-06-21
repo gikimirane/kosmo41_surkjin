5238aac138f4dda6054b0cde97f68e21da536831
surkjin(KOSMO) <surkjin@gmail.com>
2018-06-21 09:50:01 +0900

    자바수업

---
+++ Java/ch01/LineComment.java
@@ -0,0 +1,17 @@
+//파일이름: BlockComment
+//작 성 자: 정 석진
+
+class LineComment
+{
+	public static void main(String[] args)
+	{
+		System.out.println(7); // 다음은 단순한 정수의 출력 
+		
+		System.out.println(3.15);
+		System.out.println("3 + 5" + " 의 연산결과는 8 입니다");
+		System.out.println("3 + 5 = " + 8);
+		System.out.println(3.15 + "는 실수 입니다");
+		
+		System.out.println(3+5); // 다음은 덧셈 결과의 출력 
+	}
+}
