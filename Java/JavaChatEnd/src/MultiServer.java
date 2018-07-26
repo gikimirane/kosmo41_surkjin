f3bbe7850da443909c932a2091f6c84c55f88fa2
surkjin(KOSMO) <surkjin@gmail.com>
2018-07-26 20:25:33 +0900

    채팅-멀티서버수정

--- Java/JavaChatEnd/src/MultiServer.java
+++ Java/JavaChatEnd/src/MultiServer.java
@@ -6,7 +6,9 @@ import java.util.*;
 public class MultiServer {
 	ServerSocket serverSochet = null;
 	Socket socket = null;
+	Connection con=null;
 	Map<String, PrintWriter> clientMap;
+	int room_no = 0;
 	
 	public MultiServer() {
 		clientMap = new HashMap<String, PrintWriter>();
@@ -36,14 +38,54 @@ public class MultiServer {
 		}
 	}
 		
-	public void sendAllMsg(String msg) {
-	
+	public void sendAllMsg(String name, String msg) {
+			
+		//서버 금칙어 처리
+		String[] str = msg.split(" ");
+		PreparedStatement pstmt=null;
+		String sql = "";
+		int banWord = 0;
+		
+		for(int i=0; i<str.length; i++) {
+			sql = "select count(*) from van_word where mbr_nm= '$system$' and word = '"+str[i].trim()+"'";		
+			try {
+				pstmt = con.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery();
+				while(rs.next())  	banWord = rs.getInt(1);
+				if(banWord > 0) {
+					clientMap.get(name).println(str[i].trim() + "는 금칙어입니다.");
+					return;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}			
+		}
+					
 		Iterator<String> it = clientMap.keySet().iterator();
-		String name="";
+		String mName="", blk_nm="";
 		while(it.hasNext()) {
 			try {
-				name = it.next();
-				PrintWriter it_out = (PrintWriter)clientMap.get(name);
+				mName = it.next();
+				
+				//개인금칙어 처리
+				for(int i=0; i<str.length; i++) {
+					banWord = 0;
+					sql = "select count(*) from van_word where mbr_nm='"+mName+"' and word ='"+str[i].trim()+"'";
+					pstmt = con.prepareStatement(sql);
+					ResultSet rs = pstmt.executeQuery();
+					while(rs.next())  	banWord = rs.getInt(1);
+					if(banWord > 0) break;
+				}
+				if(banWord > 0)		continue;
+				
+				//대화차단자 처리
+				sql = "select distinct mbr_nm from block_list where blk_nm='"+mName+"'";
+				pstmt = con.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery();
+				while(rs.next()) { 	blk_nm = rs.getString(1);}
+				if(mName.equalsIgnoreCase(blk_nm))	continue; 
+				
+				PrintWriter it_out = (PrintWriter)clientMap.get(mName);
 				it_out.println("["+name+"] "+ msg);
 			}catch(Exception e) {
 				e.printStackTrace();
@@ -76,24 +118,23 @@ public class MultiServer {
 		
 		@Override
 		public void run() {
-			Connection con=null;
+//			Connection con=null;
 			String name = "";
 			try {
 				con = ConnectionPool.getConnection("env " + noThread);
-				con.setAutoCommit(false);
+				//con.setAutoCommit(false);
 				String sql = "select sysdate from dual";
 				PreparedStatement pstmt = con.prepareStatement(sql);
 				ResultSet rs = pstmt.executeQuery();
 				while(rs.next()) 
 					System.out.println(rs.getDate(1) + " : " + noThread + " : " + con);
-				//pstmt.close();
 				ConnectionPool.listCacheInfos();
 				
 				while(true) {
-					name = in.readLine();
-					int[] check_f = check_member(con, name);
+					name = in.readLine().trim();
+					int[] check_f = check_member(name);
 					if(check_f[1]==1)	
-						out.println(name + "-> 블랙리스트 대상자입니다. 접속할 수 없습니다");	
+						out.println(name + "-> 블랙리스트 대상자입니다. 접속할 수 없습니다.");	
 					else if(check_f[0]==1) 
 						out.println(name + 	": 동일한 이름이 접속되어 있습니다\n"
 										 +	"다른 이름으로 접속해 주십시요.");
@@ -101,7 +142,7 @@ public class MultiServer {
 				}
 				
 				clientMap.put(name, out);
-				sendAllMsg("님이 입장하셨습니다.");	
+				sendAllMsg(name, "님이 입장하셨습니다.");	
 				System.out.println("현재 접속자 수는 " + clientMap.size() + "명 입니다.");
 
 				String s = "";
@@ -109,12 +150,17 @@ public class MultiServer {
 					s = in.readLine();
 					System.out.println(s);
 					
-					if(s.equals("/list")) 										showList(name);
-					else if(s.length()>3 && s.substring(0, 3).equals("/no"))	blockList(con, name, s, out);
-					else if(s.length()>3 && s.substring(0, 3).equals("/to"))	
-																				talkToOne(name, s);
-					else														sendAllMsg(s);
-
+					if(s.equals("/list")) 						showList(name);
+					else if(s.equals("/lsrm"))					listRoom(name);
+					else if(s.substring(0, 3).equals("/to"))	talkToOne(name, s);
+					else if(s.substring(0, 4).equals("/blk"))	blockList(name, s);
+					else if(s.substring(0, 4).equals("/ban"))	banWord(name, s);					
+					else if(s.substring(0, 5).equals("/noti"))	notifyMsg(name, s);
+					else if(s.substring(0, 6).equals("/mkorm"))	makeRoom(name, s, "0");
+					else if(s.substring(0, 6).equals("/mksrm"))	makeRoom(name, s, "1");
+					else										sendAllMsg(name, s);
+					
+//					else if(s.length()>4 && s.substring(0, 3).equals("/to"))	
 ///					if(s.equals(name+"=>"+"/list")) 	showList(name);
 //					else if(s.length()>name.length()+5 && s.substring(name.length()+2, name.length()+2+3).equals("/to"))	
 //														talkToOne(name, s);
@@ -127,15 +173,11 @@ public class MultiServer {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}finally {
-				System.out.println("sleep..." + noThread);
-				try {
-					Thread.sleep(1000);
-				}catch(Exception e){}
 				
 				clientMap.remove(name);
-				sendAllMsg("님이 퇴장하셨습니다.");
+				sendAllMsg(name, "님이 퇴장하셨습니다.");
 				System.out.println("현재 접속자 수는 " + clientMap.size() + "명 입니다.");
-				deleteMbr(con, name);
+				deleteMbr(name);
 				try {
 					in.close();
 					out.close();	
@@ -143,89 +185,230 @@ public class MultiServer {
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
+				
+				System.out.println("sleep..." + noThread);
+				try {
+					Thread.sleep(1000);
+				}catch(Exception e){}			
 			}
 		}
 	}
 	
-	public int[] check_member(Connection con, String name) {
-		int[] count = {0, 0};
+	public static boolean isNumber(String input) {
+		  try{Integer.parseInt(input);	  	return true;
+		  }catch (NumberFormatException e){ return false;}
+	}
+	
+	public void listRoom(String name) {
+		String sql =  "select to_char(rum_no,'99')||'.'||room_nm||decode(open_cd, '0', '[공개방]','[비공개방]') "
+					+ "from room_list order by rum_no";
 		try {
-			String sql = "select rownum, to_number(check_f)  from member_nm " 
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			String str=null;
+			while(rs.next()) { 	clientMap.get(name).print(rs.getString(1)+"\t");}
+			clientMap.get(name).println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void makeRoom(String name, String s, String open_cd) {
+		String[] str = s.split(" ");
+		if(open_cd.equals("0") && (str.length != 3 || !(isNumber(str[2])))) {	
+			clientMap.get(name).println("'/mkorm 방이름 정원' 형식으로 해주세요.");
+			return;
+		}
+		if(open_cd.equals("1") && (str.length != 4 || !(isNumber(str[2])))) {	
+			clientMap.get(name).println("'/mksrm 방이름 정원 비밀번호' 형식으로 해주세요.");
+			return;
+		}
+		
+		try {
+			String sql = "select count(*) from room_list where room_nm = '"+str[1].trim()+"'";
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			int cnt =0;
+			while(rs.next()) { 	cnt = rs.getInt(1);}
+			if(cnt == 0) {
+				sql = "insert into room_list values(rum_no_seq.nextval, '"+str[1].trim()+"', to_number('"+str[2].trim()+"'),"
+						+ "'"+open_cd+"', decode('"+open_cd+"','0', null, '"+str[str.length-1].trim()+"'), '"+name+"')";  
+				pstmt = con.prepareStatement(sql);
+				pstmt.executeUpdate();
+				clientMap.get(name).println(str[1].trim() + "방이 만들어졌습니다.");
+			}
+			else	clientMap.get(name).println(str[1].trim() + "의 방이 이미 있습니다. 다른 방이름으로 다시 해주세요.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int[] check_member(String name) {
+		int[] check = {0, 0};
+		try {
+			String sql = "select rownum, to_number(black_f)  from member_nm " 
 						+"where mbr_nm = '"+name+"'";
 			PreparedStatement pstmt = con.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) { 	count[0] = rs.getInt(1); count[1] = rs.getInt(2);}
-			if(count[0]==0) {
+			while(rs.next()) { 	check[0] = rs.getInt(1); check[1] = rs.getInt(2);}
+			if(check[0]==0) {
 				sql = "insert into member_nm(mbr_nm) values('"+name+"')";  
 				pstmt = con.prepareStatement(sql);
 				pstmt.executeUpdate();
-				con.commit();
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
-		return count;
+		return check;
+	}
+	
+	public void notifyMsg(String name, String msg) {
+		
+		String[] str = msg.split(" ");
+		if(str.length == 1) {
+			clientMap.get(name).println("'/noti 공지내용' 형식으로 해주세요.");
+			return;
+		}
+		
+		Iterator<String> it = clientMap.keySet().iterator();
+		while(it.hasNext()) {
+			try {
+				PrintWriter it_out = (PrintWriter)clientMap.get(it.next());
+				it_out.println("[공지사항] " + msg.substring(6).trim());
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}	
 	}
 	
 	public void showList(String name) {
-		
-			Set<String> k = clientMap.keySet();
-			PrintWriter n_out = (PrintWriter)clientMap.get(name);
-			n_out.println("현재 접속자입니다("+ clientMap.size()+"명).");
-			//for(String s : k )	n_out.println(s) ;
-			n_out.println(k);
+			
+		Set<String> k = clientMap.keySet();
+		PrintWriter n_out = (PrintWriter)clientMap.get(name);
+		n_out.println("현재 접속자입니다("+ clientMap.size()+"명).");
+		//for(String s : k )	n_out.println(s) ;
+		n_out.println(k);
 	}
 	
-	public void blockList(Connection con, String name, String s, PrintWriter out) {
+	@SuppressWarnings("resource")
+	public void blockList(String name, String s) {
+		String[] s1 = s.split(" ");
+		if(s1.length != 2) {	
+			clientMap.get(name).println("'/blk 상대방이름' 형식으로 해주세요.");
+			return;
+		}
+		
 		try {
-			String[] s1 = s.split(" ");
-			String sql = "select count(*) from block_list where mbr_nm='"+name+"' and blk_nm='"+s1[1]+"'";
+			
+			String sql = "select count(*) from block_list where mbr_nm='"+name+"' and blk_nm='"+s1[1].trim()+"'";
 			PreparedStatement pstmt = con.prepareStatement(sql);
 			ResultSet rs = pstmt.executeQuery();
 			int cnt = 0;
 			while(rs.next()) 	cnt = rs.getInt(1);
 			if(cnt==0) {
-				sql = "insert into block_list values('"+name+"', '"+s1[1]+"')";
+				sql = "insert into block_list values('"+name+"', '"+s1[1].trim()+"')";
 				pstmt = con.prepareStatement(sql);
 				pstmt.executeUpdate();
-				con.commit();
-				out.println(s1[1] + "와의 대화를 차단합니다.");
+				clientMap.get(name).println(s1[1] + "와의 대화를 차단합니다.");
 			}
 			else {
-				sql = "delete from block_list where mbr_nm='"+name+"' and blk_nm='"+s1[1]+"')";
+				sql = "delete from block_list where mbr_nm='"+name+"' and blk_nm='"+s1[1].trim()+"')";
 				pstmt = con.prepareStatement(sql);
 				pstmt.executeUpdate();
-				con.commit();
-				out.println(s1[1] + "와의 차단을 해제합니다.");	
+				clientMap.get(name).println(s1[1] + "와의 차단을 해제합니다.");	
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void deleteMbr(Connection con, String name) {
+	public void deleteMbr(String name) {
 		try {
-			String sql = "delete from member_nm where mbr_nm = '"+name+"'";
+			String sql = "delete from member_nm where mbr_nm = '"+name+"' and black_f ='0'";
 			PreparedStatement pstmt = con.prepareStatement(sql);
 			pstmt.executeUpdate();
 			sql = "delete from block_list where mbr_nm = '"+name+"'";
 			pstmt = con.prepareStatement(sql);
 			pstmt.executeUpdate();
-			con.commit();
+			sql = "delete from van_word where mbr_nm='"+name+"'";  
+			pstmt = con.prepareStatement(sql);
+			pstmt.executeUpdate();
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void talkToOne(String nm, String s) {
+	@SuppressWarnings("resource")
+	public void banWord(String name, String s){
+		String[] str = s.split(" ");
+		if(str.length == 1) {
+			clientMap.get(name).println("'/ban 금칙어' 형식으로 해주세요.");
+			return;
+		}
 		
-//		String[] s1 = s.substring(nm.length()+2).split(" ");
+		PreparedStatement pstmt;
+		try {
+			String sql = "select count(*) from van_word where mbr_nm != '"+name+"' and word='"+s.substring(5).trim()+"'"; 
+			pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			int cnt = 0;
+			while(rs.next()) 	cnt = rs.getInt(1);
+			if(cnt > 0) {
+				clientMap.get(name).println(s.substring(5).trim() + "는 이미 서버금칙어입니다.");
+			}
+			else{
+				sql = "select count(*) from van_word where mbr_nm = '"+name+"' and word='"+s.substring(5).trim()+"'"; 
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				int cnt1 = 0;
+				while(rs.next()) 	cnt1 = rs.getInt(1);
+				if(cnt1 == 0) {
+					sql = "insert into van_word values('"+name+"','"+s.substring(5).trim()+"')";  
+					pstmt = con.prepareStatement(sql);
+					pstmt.executeUpdate();
+					clientMap.get(name).println(s.substring(5).trim() + "는 금칙어에 포함됩니다.");
+				}
+				else {
+					sql = "delete from van_word where mbr_nm='"+name+"' and word='"+s.substring(5).trim()+"'";  
+					pstmt = con.prepareStatement(sql);
+					pstmt.executeUpdate();
+					clientMap.get(name).println(s.substring(5).trim() + "는 금칙어에서 해제합니다.");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void talkToOne(String name, String s) {
+		
 		String[] s1 = s.split(" ");
+		if(s1.length == 1) {
+			clientMap.get(name).println("'/to 상대방 [메시지]' 형식으로 해주세요.");
+			return;
+		}
+		String blk_nm = null;
 		
-		PrintWriter out = (PrintWriter)clientMap.get(s1[1]);
-//		String s2 = "(귓속말)" + nm + "=>" + s.substring(nm.length()+2+ s1[0].length()+s1[1].length()+2);
-		String s2 = "(귓속말)" + nm + "=>" + s.substring(4);		
-		out.println(s2) ;	
+		try {
+			String sql = "select distinct blk_nm from block_list where mbr_nm='"+s1[1].trim()+"'";
+			PreparedStatement pstmt;
+			pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) { 	blk_nm = rs.getString(1);}
+			if(name.equalsIgnoreCase(blk_nm)) {
+				PrintWriter out = (PrintWriter)clientMap.get(name);
+				out.println("[" + s1[1].trim() +"] 님이 대화차단자로 설정하여 귓속말을 할 수 없습니다.");
+			}
+			else {
+				PrintWriter out = (PrintWriter)clientMap.get(s1[1].trim());
+//				String s2 = "(귓속말)" + nm + "=>" + s.substring(nm.length()+2+ s1[0].length()+s1[1].length()+2);
+				out.println("[" + name +"] (귓속말)" + s.substring(4).trim());		
+//				out.println(s2) ;	
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 	}
 	
 	public static void main(String[] args){
